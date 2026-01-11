934847125d62ab8ec517a9c9eb6805f5ce9e49e4
Joran Schols <schols.joran@gmail.com>
2026-01-11 10:13:30 +0100

    cleanup

--- src/app/features/forecast/forecast-page/forecast-page.component.ts
+++ src/app/features/forecast/forecast-page/forecast-page.component.ts
@@ -1,15 +1,13 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { WeatherApiService } from '../../../services/weather-api.service';
-import { ForecastCardsComponent } from "../forecast-cards/forecast-cards.component";
 import { LocationSearchComponent } from "../../location-search/location-search.component";
-import { BestTwoHoursComponent } from "../best-two-hours/best-two-hours.component";
 import { CommonModule } from '@angular/common';
 import { ForecastMatrixComponent } from "../forecast-matrix/forecast-matrix.component";
 import { LocationService } from '../../../services/location.service';
 
 @Component({
   selector: 'app-forecast-page',
-  imports: [CommonModule, ForecastCardsComponent, LocationSearchComponent, ForecastMatrixComponent],
+  imports: [CommonModule, LocationSearchComponent, ForecastMatrixComponent],
   templateUrl: './forecast-page.component.html',
   styleUrl: './forecast-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
